7652fe58f244f9bebaaf7bda72d5b53249dd5ef1
mahmudulhasan0 <113965427+mahmudulhasan0@users.noreply.github.com>
2022-09-27 23:36:41 +0530

    Update Lab2.cpp

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,17 +1,74 @@
 #include <iostream>
 using namespace std;
-int main ()
-{
-    int arr1 []={1,2,3,4,5};
-    int arr2 []= {6,7,8};
-    int arr3 [8];
-    for (int i=0; i<5; i++)
-    { arr3[i]=arr1[i];
+
+int main() {
+
+     int arr1[] = {1,2,3,4,5};
+     int arr2[] = {1,2,8};
+     int arr3[] =  {1,2,3,4,5};
+     int arr4[] = {6,7,8};
+
+       cout << "First Array: ";
+      for (int i=0; i< 5; i++) {
+           cout<< arr1[i] << ' ';
         }
-        for (int i=5; i<8; i++)
-    { arr3[i]=arr2[i-5];
+
+       cout <<endl << "Second Array: ";
+      for (int i=0; i< 3; i++) {
+            cout<< arr2[i] << ' ';
         }
-  for (int i=(8-1); i>=0; i--)
-    { cout<<arr3[i]<<' ' ;
+      cout<<endl;
+
+ cout <<"Common Element: ";
+      for(int i=0;i<5;i++)
+       {
+        for(int j=0;j<3;j++)
+        {
+            if(arr1[i]==arr2[j]){
+            cout<<arr1[i]<<" ";
+            }
+
+
         }
- }
+
+    }
+cout<<endl<<endl<<endl;
+     cout << "Third Array: ";
+      for (int i=0; i< 5; i++) {
+           cout<< arr3[i] << ' ';
+        }
+
+       cout <<endl << "Fourth Array: ";
+      for (int i=0; i< 3; i++) {
+            cout<< arr4[i] << ' ';
+        }
+      cout<<endl;
+
+cout <<"Common Element: ";
+int count =0;
+      for(int i=0;i<5;i++)
+       {
+        for(int j=0;j<3;j++)
+        {
+            if(arr3[i]==arr4[j]){
+            cout<<arr1[i]<<" ";
+            count++;
+            break;
+            }
+        }
+    }
+
+  if(count == 0){
+                cout<<"No common element";
+
+            }
+
+    return 0;
+    }
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
