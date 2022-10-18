dfd3775ccd15d2fcd7f7b845fee40cb9994779bc
mahmudulhasan0 <113965427+mahmudulhasan0@users.noreply.github.com>
2022-10-18 13:53:00 +0600

    Add files via upload

---
+++ Problem-1_lab_5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n,tmp, arr[j] ;
+for(int i=0; i<(n-1); i++)
+{for (int j=0; j<(n-i-1); j++)
+{
+if ( arr[j]>arr [j+1])
+{
+tmp= arr [j];
+arr[j] = arr [j+1];
+arr[j+1]=tmp;
+
+}
+}}
+}
